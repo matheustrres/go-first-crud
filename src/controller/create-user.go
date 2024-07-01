9a050435732a0c96ff6a10e82b88fe4cdec903e5
Matheus Torres <torresmiranda.rj@gmail.com>
2024-07-01 10:24:55 -0300

    feat: create user request models

--- src/controller/create-user.go
+++ src/controller/create-user.go
@@ -1,5 +1,25 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/matheustrres/go-first-crud/src/config/rest_errors"
+	"github.com/matheustrres/go-first-crud/src/controller/models/request"
+)
+
+func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_errors.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()),
+		)
+
+		c.JSON(restErr.Code, restErr)
+
+		return
+	}
+
+	fmt.Println(userRequest)
+}
