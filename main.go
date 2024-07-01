d56115e4e0c3c955d76df820b877c03807445320
Matheus Torres <torresmiranda.rj@gmail.com>
2024-07-01 10:08:56 -0300

    feat: initialize gin router

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/matheustrres/go-first-crud/src/controller/routes"
 )
 
 func main() {
@@ -15,5 +15,17 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	fmt.Println(os.Getenv("STAGE"))
+	/**
+	* Can use gin.New or gin.Default. Both are valid options but has its differences:
+	*
+	* - gin.New: initialize a router without any middleware
+	* - gin.Default: initialize a router with with logger and (panic) recovery middlewares
+	 */
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
