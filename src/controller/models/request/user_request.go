0a75c0ddb0601192fea5f64d4a43dad96d817838
Matheus Torres <torresmiranda.rj@gmail.com>
2024-07-01 10:38:12 -0300

    feat: validate user request fields

--- src/controller/models/request/user_request.go
+++ src/controller/models/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Age      int8   `json:"age"`
+	Name     string `json:"name" binding:"required,min=4,max=100"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,containsany=!@#$%&()*+-"`
+	Age      int8   `json:"age" binding:"required,min=18,max=100"`
 }
